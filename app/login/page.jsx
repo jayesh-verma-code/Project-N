4e256bcafefd6aad92dc7b1b14c9f6552b86087f
jayesh-verma-code <jayeshvermaformalwork@gmail.com>
2025-08-30 22:57:29 +0530

    created login and signup route

---
+++ app/login/page.jsx
@@ -0,0 +1,14 @@
+"use client";
+import React, { useRef } from "react";
+
+
+const page = () => {
+  
+  return (
+    <div suppressHydrationWarning>
+        Login page
+    </div>
+  );
+};
+
+export default page;
---
+++ app/signup/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+import React, { useRef } from "react";
+
+
+const page = () => {
+  
+  return (
+    <div suppressHydrationWarning>
+        Signup page
+    </div>
+  );
+};
+
+export default page;
